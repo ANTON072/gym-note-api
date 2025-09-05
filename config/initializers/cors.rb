071a948712e2a03dc6f2745ecf8eba2ce440e999
Katsushi OUGI <ougi@to-r.net>
2025-09-05 13:45:27 +0000

    CORS設定を更新し、開発および本番環境のオリジンを適切に設定。rack-corsをGemfileに追加。

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,24 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # 環境に応じて許可するオリジンを設定
+    allowed_origins = case Rails.env
+    when "development"
+      [ "http://localhost:3001", "http://localhost:3000" ]
+    when "production"
+      # TODO: 本番環境のフロントエンドURLに置き換える
+      [ "https://app.example.com" ]
+    else
+      [ "http://localhost:3001" ]
+    end
+
+    origins(*allowed_origins)
+
+    resource "*",
+      headers: :any,
+      methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+      credentials: true  # 認証情報（Cookie等）を含むリクエストを許可
+  end
+end
