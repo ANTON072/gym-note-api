a1e4615a21a8d85111af202d69675f0f8902c6ec
Katsushi OUGI <ougi@to-r.net>
2025-09-09 03:52:20 +0000

    WorkoutExerciseモデルとそのテストにスキーマ情報を追加し、exercise_idの一意性バリデーションを追加

--- app/models/workout_exercise.rb
+++ app/models/workout_exercise.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: workout_exercises
+#
+#  id           :bigint           not null, primary key
+#  notes        :text(65535)
+#  order_index  :integer          not null
+#  total_volume :integer          default(0), not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  exercise_id  :bigint           not null
+#  workout_id   :bigint           not null
+#
+# Indexes
+#
+#  index_workout_exercises_on_exercise_id                 (exercise_id)
+#  index_workout_exercises_on_workout_id                  (workout_id)
+#  index_workout_exercises_on_workout_id_and_exercise_id  (workout_id,exercise_id) UNIQUE
+#  index_workout_exercises_on_workout_id_and_order_index  (workout_id,order_index)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (exercise_id => exercises.id)
+#  fk_rails_...  (workout_id => workouts.id)
+#
 class WorkoutExercise < ApplicationRecord
   belongs_to :workout
   belongs_to :exercise
