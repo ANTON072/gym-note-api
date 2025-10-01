034725804c2a6daaf87ae7e25d7a8f1ff4140476
Katsushi OUGI <ougi@to-r.net>
2025-10-01 15:16:04 +0900

    localhostもcorsに許可

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -15,7 +15,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     when "development"
       [ web_local_origin ]
     when "production"
-      [ web_production_origin ]
+      [ web_production_origin, web_local_origin ]
     else
       [ web_local_origin ]
     end
